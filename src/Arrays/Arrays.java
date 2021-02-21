c1e4b4325d25d1575d5f6475101a30663e3ae6aa
Audu Billy <audubilly89@gmail.com>
2021-02-21 19:35:57 +0100

    updates

--- src/Arrays/Arrays.java
+++ src/Arrays/Arrays.java
@@ -8,35 +8,39 @@ public class Arrays {
     public static void main(String[] args) {
 
         int[] myIntegers = getIntegers(5);
+       int [] sorted = sortIntegers(myIntegers);
+       printArray(sorted);
+        System.out.println("the average of the values are " + Average(myIntegers));
 
     }
 
     public static int[] getIntegers(int capacity) {
         int[] array = new int [capacity];
-        System.out.println("Enter" + capacity +"Integer values:\r");
-        for(int i = 0; i <= array.length; i++){
+        System.out.println("Enter " + capacity +" Integer values: \r");
+        for(int i = 0; i < array.length; i++){
             array[i] = scanner.nextInt();
         }
         return array;
     }
 
     public static void printArray(int [] array){
-        for(int i = 0; i <= array.length; i++){
-            System.out.println("elements" + i + "contents" + array[i] );
+        for(int i = 0; i < array.length; i++){
+            System.out.println("elements " + i + " contents " + array[i] );
         }
     }
 
 
     public static int[] sortIntegers(int [] array){
         int [] sortedArray = new int[array.length];
-        for(int i = 0; i <= array.length; i++){
+        for(int i = 0; i < array.length; i++){
            sortedArray[i] = array[i];
         }
         boolean flag = true;
         int temp;
         while(flag){
-            for(int i = 0; i <= sortedArray.length- 1; i++){
-                if(sortedArray[i] < sortedArray[i=1]){
+            flag = false;
+            for(int i = 0; i < sortedArray.length-1; i++){
+                if(sortedArray[i] < sortedArray[i+1]){
                     temp = sortedArray[i];
                     sortedArray[i] = sortedArray[i+1];
                     sortedArray[i+1] = temp;
@@ -46,6 +50,14 @@ public class Arrays {
             }
         }
         return sortedArray;
+    }
+
+    public static double Average(int [] array){
+        int sum = 0;
+        for(int i = 0 ; i < array.length ; i++){
+             sum += array[i];
+        }
+        return (double) sum / (double) array.length;
     }
 
 }
