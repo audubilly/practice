8d6c22417548a55dae0bcd8edaa7e058261a7bd9
Audu Billy <audubilly89@gmail.com>
2021-02-22 13:08:29 +0100

    reverse Array

--- src/Arrays/ReverseArrays.java
+++ src/Arrays/ReverseArrays.java
@@ -1,8 +1,13 @@
 package Arrays;
 
+import java.util.Arrays;
+
 public class ReverseArrays {
     public static void main(String[] args) {
-        int []
+        int [] myArray = {68,45,70,76,90,22,50};
+        System.out.println("the array is " + Arrays.toString(myArray));
+         reverseArray(myArray);
+        System.out.println("the reversed array is " + Arrays.toString(myArray));
     }
 
     private static void reverseArray(int [] array){
@@ -11,8 +16,8 @@ public class ReverseArrays {
 
         for(int i = 0; i < halfLength ; i++){
             int temp = array[i];
-            array[i] = array[maxIndex - 1];
-            array[maxIndex - 1] = temp;
+            array[i] = array[maxIndex - i];
+            array[maxIndex - i] = temp;
         }
 
     }
