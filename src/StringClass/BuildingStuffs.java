a0508f44fb50057905350fcd6e23a48fd291a4f8
Audu Billy <audubilly89@gmail.com>
2021-03-06 21:36:32 +0100

    String builder updates

--- src/StringClass/BuildingStuffs.java
+++ src/StringClass/BuildingStuffs.java
@@ -15,10 +15,25 @@ public class BuildingStuffs {
         System.out.printf("buffer1 = \"%s\"%n", builder1);
         System.out.printf("buffer2 = \"%s\"%n", builder2);
         System.out.printf("buffer3 = \"%s\"%n", builder3);
-        System.out.println(builder1);
-        System.out.println(builder3);
+        System.out.println("**********************************");
+//        System.out.println(builder1);
+//        System.out.println(builder3);
 //        System.out.println(builder1.append(builder2));
 
+
+        StringBuilder buffer = new StringBuilder("hey young one, the world is yours");
+
+
+        System.out.println("length is : "  + buffer.length());
+        System.out.println("capacity is : " + buffer.capacity());
+        System.out.println("the To string is: " + buffer.toString());
+
+        buffer.ensureCapacity(50);
+        System.out.println("ok capacity is : " + buffer.capacity());
+        buffer.setLength(12);
+        System.out.println("the new length is : " + buffer.length());
+        System.out.println("new capacity is : " + buffer.capacity());
+        System.out.println(buffer.reverse());
 
 
     }
