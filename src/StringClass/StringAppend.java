a137ad00556f506466d719ab7e275f5357c2e48d
Audu Billy <audubilly89@gmail.com>
2021-03-07 14:14:30 +0100

    String Append

---
+++ src/StringClass/StringAppend.java
@@ -0,0 +1,22 @@
+package StringClass;
+
+public class StringAppend {
+
+    public static void main(String[] args) {
+
+        Object objectRef = "hello";
+        String string = "goodbye";
+        char[] charArray = {'a', 'b', 'c', 'd', 'e', 'f'};
+        boolean booleanValue = true;
+        char characterValue = 'Z';
+        int integerValue = 7;
+        long longValue = 10000000000L;
+        float floatValue = 2.5f;
+        double doubleValue = 33.333;
+
+        StringBuilder lastBuffer = new StringBuilder("last buffer");
+        StringBuilder builder1 = new StringBuilder();
+
+
+    }
+}
